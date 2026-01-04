af4bda667d67a60020961e91e82677a3075714d2
Afreh krissNtim <afrehkriss2101@gmail.com>
2026-01-04 17:32:57 +0000

    added english psalms

--- src/main.py
+++ src/main.py
@@ -104,7 +104,7 @@ def save_json(path, data):
 OT_ORDER = [
     'Genesis','Exodus','Leviticus','Numbers','Deuteronomy','Joshua','Judges','Ruth',
     '1 Samuel','2 Samuel','1 Kings','2 Kings','1 Chronicles','2 Chronicles','Ezra','Nehemiah','Esther',
-    'Job','Psalms','Proverbs','Ecclesiastes','Song of Solomon','Isaiah','Jeremiah','Lamentations',
+    'Job','Psalm','Proverbs','Ecclesiastes','Song of Solomon','Isaiah','Jeremiah','Lamentations',
     'Ezekiel','Daniel','Hosea','Joel','Amos','Obadiah','Jonah','Micah','Nahum','Habakkuk','Zephaniah','Haggai','Zechariah','Malachi'
 ]
 
